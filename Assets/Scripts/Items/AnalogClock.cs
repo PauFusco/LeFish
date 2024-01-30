f42ac5d497d9549efb0ee20ba3b6c161e163a33d
Historn <arnaujiga@gmail.com>
2024-01-30 16:01:44 +0100

    Clock and fish reality animation

---
+++ Assets/Scripts/Items/AnalogClock.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalogClock : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _hourHand;
+    
+    [SerializeField]
+    private Transform _minuteHand;
+    
+    [SerializeField]
+    private Transform _secondHand;
+
+    private int _previousSeconds;
+    private int _timeInSeconds;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ConvertTimeToSeconds();
+        RotateClockHands();
+    }
+
+    private int ConvertTimeToSeconds()
+    {
+        int currentSeconds = DateTime.Now.Second;
+        int currentMinutes = DateTime.Now.Minute;
+        int currentHour = DateTime.Now.Hour;
+
+        if (currentHour >= 12)
+        {
+            currentHour -= 12;
+        }
+
+        _timeInSeconds = currentSeconds + (currentMinutes*60) + (currentHour * 60 * 60);
+        return _timeInSeconds;
+    }
+    private void RotateClockHands()
+    {
+        float secondHandPerSecond = 360f / 60f;
+        float minuteHandPerSecond = 360f / (60f * 60f);
+        float hourHandPerSecond = 360f / (60f * 60f * 12f);
+
+        if (_timeInSeconds != _previousSeconds)
+        {
+            _secondHand.localRotation = Quaternion.Euler(0, 0, _timeInSeconds * secondHandPerSecond);
+            _minuteHand.localRotation = Quaternion.Euler(0, 0, _timeInSeconds * minuteHandPerSecond);
+            _hourHand.localRotation = Quaternion.Euler(0, 0, _timeInSeconds * hourHandPerSecond);
+        }
+        _previousSeconds = _timeInSeconds;
+    }
+}
